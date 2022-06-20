91d439bce6ab0bec79a8c075b3f07733cc75511c
Daniel Escobar <daanielescobar@gmail.com>
2022-06-20 01:26:30 -0500

    Add every level of difficulty.

--- components/question.js
+++ components/question.js
@@ -1,27 +1,68 @@
-export default function Questions({player, bucketURL, playerList}){
+import { useState } from "react"
+
+export default function Questions({player, bucketURL, playerList, onSelection, children, onReachedMaxScore, level}){
+
+    const [score, setScore] = useState('0')
+
     function checkIfCorrect(clicked, image){
         if(clicked == image){
             //reload component
+            //add 1 point
             console.log('you guessed!')
+            setScore(score + 1)
+            onSelection()
+            if( score > 5 ){
+                onReachedMaxScore()
+                setScore(0)
+            }
         }else{
             console.log('you are wrong!')
+            setScore(0)
+            //restart score
         }
     }
 
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
+
+    var randomizedList = shuffle(playerList)
+
+    console.log('playerList: ', playerList)
 
     return(
         <div className="z-10 h-screen w-screen flex align-middle items-center  " >
             <div className=" " >
+                <h1 className="text-white " >Nivel: { level + 1 }</h1>
                 <h1 className="text-white text-7xl" >¿Cual es su nombre?</h1>
                 <div>
                     {
-                        playerList.map((p) => 
-                            <div key={p} className="  mt-2 
-                            border-2 border-white rounded-md
-                            text-2xl text-white 
+                        randomizedList.map((p, i) => 
+                            <div key={i} className="  
                             " >
                                 <button 
-                                    className=""
+                                    className="mt-2 
+                                    border-2 border-white rounded-md
+                                    text-2xl text-white
+                                    hover:bg-white
+                                    hover:text-black
+                                    transition 
+                                    w-72
+                                    "
                                     onClick={() => checkIfCorrect(p, player)}
                                 >
                                     {p}
@@ -29,6 +70,9 @@ export default function Questions({player, bucketURL, playerList}){
                             </div>
                         )
                     }  
+                </div>
+                <div className="font-bold text-green-500 text-4xl tex-center " >
+                    Score: { score }
                 </div>
             </div>
         </div>
--- pages/play.js
+++ pages/play.js
@@ -1,19 +1,40 @@
-import { useEffect, useContext, useState } from "react";
-import Questions from "../images/components/question";
+import { useEffect, useState } from "react";
+import Questions from "../components/question";
 import Image from "next/image";
 
 export default function Play(){
-    const pro1 = ['Acce', 'Ackerman', 'Aloned', 'Andresx', 'Arce', 'Buggax', 'Dimitry', 'Gonto', 'Grell', 'Jauny', 'Jirall']
-    const comodines = ['Duki', 'Messi','Goku', 'WillyRex', 'Misrra']
-    
+    const level1 = ['Acce', 'Ackerman', 'Aloned', 'Andresx', 'Arce', 'Buggax', 'Dimitry', 'Gonto', 'Grell', 'Jauny', 'Jirall', 'Josedeodo', 'Leza', 'Newbie', 'Oddie', 'Pancake', 'Plugo',    'Rakyz', 'Relic', 'Seiya', 'Shadow', 'Slow', 'Sookie', 'Straight', 'Tierwulf', 'Wara', 'Whitelotus', 'Yeti']
+    const level2 = ['AlanQ', 'Baula', 'Careta', 'Ceo', 'Chunilda', 'Cody', 'Cotopaco', 'Elmo', 'Fallen', 'Gonzo', 'JJ', 'Kala', 'Kalipso', 'Kiefer', 'Milky', 'Monrrow', 'Renn', 'Shuhari', 'Solidsnake', 'Toplop', 'froststrike']
+    const level3 = ['1an', 'Biblos', 'Bola', 'Brayaron', 'Chiquito', 'Corsario', 'Crecre', 'Demizos', 'Emp', 'Fahren', 'Fix', 'Gavotto', 'Juliostito', 'Limitations', 'Mantarraya', 'Mataz', 'Pan', 'Skeeto', 'Skin', 'Tyr', 'Tyrone', 'Unforgiven', 'Zerito', 'Zeypher', 'Zombyra', 'Zothve']
+    const level4 = ['Bardito', 'Beto', 'Camilo', 'Dye', 'Enga', 'Flare', 'Follow', 'Genthix', 'Liquiddiego', 'Maggical', 'Mariome', 'Messi', 'Nerzhul', 'Pierre', 'QQ', 'Revehaza', 'Soren', 'Tommy', 'Twins', 'Xypherz', 'Zelt']
+    const level5 = ['Akunma', 'Aquasonic', 'DCStar', 'Fatorix', 'Gastruks', 'Gralou', 'Horus', 'Igor', 'Igor', 'Jcyter', 'Khynm', 'Kz', 'Lesmart', 'Manu', 'Minibestia', 'Mister G', 'Nate', 'Nobody', 'Rafamaik', 'Snaker', 'Ukkyr', 'Zeicro']
+    const level6 = ['Arfyss', 'Daiky', 'Enatsu', 'Feitan', 'Fragio', 'Icebox', 'Kindless', 'Koughi', 'Kouzze', 'Kreshtdoo', 'Lyg', 'Madblade', 'Mun', 'Pieck', 'Shmebu', 'Shyro', 'Snok', 'Symebo', 'Tulz', 'Wikko', 'Xuradel']
+    const comodines = ['Duki','Goku', 'WillyRex', 'Misrra', 'Chispostin', 'Pozzo', 'Tucky']
+
+    const levelsList = [level1, level2, level3, level4, level5, level6]
+
     const [bucketURL, setBucketURL] = useState('')
     const [proName, setProName] = useState('')
+    const [options, setOptions] = useState([])
+    const [level, setLevel] = useState(0)
     
     function generateURL(){
-        let name = Math.floor(Math.random() * (pro1.length - 0) ) + 0;
+
+        setOptions([])
+        console.log('creating new url')
+        console.log(levelsList[level][0])
+        let name = Math.floor(Math.random() * (levelsList[level].length - 0) ) + 0;
         let num = Math.floor(Math.random() * (2 - 1) ) + 1;
-        setProName(pro1[name]) 
-        setBucketURL(`https://ndvztwbadgizyjthrjjs.supabase.co/storage/v1/object/public/pictures/images/1/${ pro1[name] }%20${ num }.png`)
+
+        setOptions([
+                levelsList[level][Math.floor(Math.random() * (levelsList[level].length - 0) ) + 0],
+                levelsList[level][Math.floor(Math.random() * (levelsList[level].length - 0) ) + 0], 
+                levelsList[level][Math.floor(Math.random() * (levelsList[level].length - 0) ) + 0], 
+                levelsList[level][name],
+        ])
+
+        setProName(levelsList[level][name]) 
+        setBucketURL(`https://ndvztwbadgizyjthrjjs.supabase.co/storage/v1/object/public/pictures/images/${ level + 1 }/${ levelsList[level][name] }%20${ num }.png`)
     }
     
     useEffect(() => {
@@ -25,13 +46,19 @@ export default function Play(){
             <div className="-z-10 fixed overflow-hidden h-screen w-screen " >
                 <Image
                     alt="Mountains"
-                    src={"https://ndvztwbadgizyjthrjjs.supabase.co/storage/v1/object/public/pictures/images/1/Seiya 1.png"}
+                    src={bucketURL || "https://ndvztwbadgizyjthrjjs.supabase.co/storage/v1/object/public/pictures/images/1/Seiya 1.png"}
                     layout="fill"
                     objectFit="cover"
                     quality={100}
                 />
             </div>
-            <Questions player={proName}  playerList={pro1}  />
+            <Questions 
+                player={proName}  
+                playerList={options} 
+                level={level}
+                onSelection={generateURL} 
+                onReachedMaxScore={ () => setLevel(level + 1)}
+            />
         </div>
     )
 }
